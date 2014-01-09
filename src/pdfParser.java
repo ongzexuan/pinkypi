80e4cf75de9444409867f62486b7fbdfb0a555ca
Ong Ze Xuan <ongzexuan@205-95.priv30.nus.edu.sg>
2014-01-09 16:58:54 +0800

    End of Lesson Commit
    -Added iText library
    -Run profile created
    -Terminal output added

---
+++ src/pdfParser.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ongzexuan
+ * Date: 9/1/14
+ * Time: 4:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class pdfParser {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Program started");
+
+        System.out.println("Program ended");
+
+    }
+
+}
