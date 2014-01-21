6a9b9e90dbe614bdebf919c4f0e2482bd4dfa251
endairA <ThreadOfEthAriadne@gmail.com>
2014-01-21 19:10:52 +0800

    Added functionality upload output
    Receives File object of output.txt and uploads data onto database

---
+++ src/FormData.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class FormData {
+	String SSEFCode;
+	LinkedList<DataSet> dataList;
+	
+	public FormData(String SSEFCode, File inputData) {
+		this.SSEFCode = SSEFCode;
+		readFromFile(inputData);
+	}
+	
+	private void readFromFile(File input) {
+		try {
+			Scanner sc = new Scanner(input);
+			StringTokenizer st;
+			String inputLine, fieldName, fieldType, fieldData;
+			while (sc.hasNextLine()) {
+				inputLine = sc.nextLine();
+				st = new StringTokenizer(inputLine, ":");
+				fieldName = st.nextToken().replaceAll("\\s", "");
+				fieldType = st.nextToken();
+				fieldData = st.nextToken();
+				
+				switch (fieldType) {
+				case "Text":
+					dataList.add(new DataSet<String>(fieldName, fieldData, true));
+					break;
+				case "Checkbox":
+					dataList.add(new DataSet<Boolean>(fieldName, (fieldData.equals("No")?false:true), false));
+					break;
+				default:
+					break;
+				}
+			}
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getSSEFCode() {
+		return SSEFCode;
+	}
+}
